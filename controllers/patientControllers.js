237a7bcc0544ca6cdab004f07296db7c01d27fa5
Divyanshu Verma <divyanshuverma08@gmail.com>
2022-12-25 00:42:04 +0530

    pdf fix

--- controllers/patientControllers.js
+++ controllers/patientControllers.js
@@ -26,22 +26,28 @@ module.exports.get_patient = async (req, res) => {
 
 module.exports.info_patient = async (req, res) => {
   const id = req.params.id;
-  const patient = await Patient.findOne({ healthID : id });
-  pdf.create(pdfTemplate(patient), {}).toFile(`${__basedir}/documents/${patient.healthID}info.pdf`, (err) => {
-    if(err) {
-        res.send(Promise.reject());
-    }
-    // res.status(200).sendFile(`${__basedir}/documents/${patient.healthID}info.pdf`)
-    var stream = fs.createReadStream(`${__basedir}/documents/${patient.healthID}info.pdf`);
-    var filename = `${patient.healthID}info.pdf`; 
-    // Be careful of special characters
-  
-    filename = encodeURIComponent(filename);
-    // Ideally this should strip them
-  
-    res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
-    res.setHeader('Content-type', 'application/pdf');
-  
-    stream.pipe(res)
-  });
+  try{
+    const patient = await Patient.findOne({ healthID : id });
+    pdf.create(pdfTemplate(patient), {}).toFile(`${__basedir}/documents/${patient.healthID}info.pdf`, (err) => {
+      if(err) {
+          console.log(err);
+          res.send(Promise.reject());
+          return;
+      }
+      // res.status(200).sendFile(`${__basedir}/documents/${patient.healthID}info.pdf`)
+      var stream = fs.createReadStream(`${__basedir}/documents/${patient.healthID}info.pdf`);
+      var filename = `${patient.healthID}info.pdf`; 
+      // Be careful of special characters
+    
+      filename = encodeURIComponent(filename);
+      // Ideally this should strip them
+    
+      res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
+      res.setHeader('Content-type', 'application/pdf');
+    
+      stream.pipe(res)
+    });
+  }catch(err){
+    res.status(401).json({error : "Patient not found !!"})
+  }
 };
