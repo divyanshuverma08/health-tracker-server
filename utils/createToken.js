1428b78bbe17d15b776bd9a881774c49f42e6b5e
Divyanshu Verma <divyanshuverma08@gmail.com>
2022-12-24 23:32:28 +0530

    fixes token

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,7 +1,4 @@
 const jwt = require("jsonwebtoken");
-const maxAge = 3 * 24 * 60 * 60;
 module.exports.createToken = (id) => {
-  return jwt.sign({ id }, process.env.SECRET_KEY, {
-    expiresIn: maxAge,
-  });
+  return jwt.sign({ id }, process.env.SECRET_KEY);
 };
